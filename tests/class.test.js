0dc43db8ec38a9f4dd4c09e0aaea64f586b0c193
Elisa Chiappini <elisabichiappini@gmail.com>
2025-03-09 00:03:42 +0100

    feat: add 3 tests

--- tests/class.test.js
+++ tests/class.test.js
@@ -22,8 +22,16 @@ test('Controlla che User contenga le proprietà name e age', () => {
     const user = new User();
     expect(user).toHaveProperty("name");
     expect(user).toHaveProperty("age");
-})
+});
 
 //controlla che il costruttore obblighi a passare le proprietà necessarie
+test('controlla che il costruttore obblighi a passare le proprietà necessarie', () => {
+    //mi aspetto che expect che scrivere new User() senza passare niente mi ritorni un errore
+    expect(() => new User()).toThrow(); //non passo niente
+    //errore anche se passo solo un parametro
+    experct(() => new User("Elisa")).toThrow(); //passo solo il nome
+    //errore se passo solo il secondo parametro
+    expect(() => new User(null, 22)).toThrow(); //passo solo l'età
+});
 //controlla le proprietà name ed age siano della giusta tipologia
 //controlla le proprietà private

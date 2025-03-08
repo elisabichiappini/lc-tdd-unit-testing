eb88f4b41fbec1e3a571d8d11958a1022d5d9247
Elisa Chiappini <elisabichiappini@gmail.com>
2025-03-08 23:48:46 +0100

    feat: go test

--- tests/class.test.js
+++ tests/class.test.js
@@ -13,6 +13,14 @@ test('creare un\'istanza della classe user', () => {
 
 //Altri test
 //Controlla che User contenga le proprietà name e age
+
+test('Controlla che User contenga le proprietà name e age', () => {
+
+    const user = new User();
+    expect(user).toHaveProperty("name");
+    expect(user).toHaveProperty("age");
+})
+
 //controlla che il costruttore obblighi a passare le proprietà necessarie
 //controlla le proprietà name ed age siano della giusta tipologia
 //controlla le proprietà private

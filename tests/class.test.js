8ca48ddca41e936e96a1198ae42cd2aab39f3536
Elisa Chiappini <elisabichiappini@gmail.com>
2025-03-09 13:22:31 +0100

    feat: fallisce il tothrow

--- tests/class.test.js
+++ tests/class.test.js
@@ -1,14 +1,16 @@
-
-    // creo istanza
-    class User {
-        constructor(name, age) {
-            this.name = name;
-            this.age = age;
+// creo istanza
+class User {
+    
+    constructor(name, age) {
+        if (!name || !age) {
+            throw new Error('Name and age are required');
         }
+        this.name = name;
+        this.age = age;
     }
+}
 // creare un'istanza della classe user
 test('creare un\'istanza della classe user', () => {
-
     const user = new User();
     //aggiungo cosa mi aspetto come risultato
     expect(user).toBeInstanceOf(User);
@@ -16,9 +18,7 @@ test('creare un\'istanza della classe user', () => {
 
 //Altri test
 //Controlla che User contenga le proprietà name e age
-
 test('Controlla che User contenga le proprietà name e age', () => {
-
     const user = new User();
     expect(user).toHaveProperty("name");
     expect(user).toHaveProperty("age");
@@ -27,11 +27,11 @@ test('Controlla che User contenga le proprietà name e age', () => {
 //controlla che il costruttore obblighi a passare le proprietà necessarie
 test('controlla che il costruttore obblighi a passare le proprietà necessarie', () => {
     //mi aspetto che expect che scrivere new User() senza passare niente mi ritorni un errore
-    expect(() => new User()).toThrow(); //non passo niente
+    expect(() => new User()).toThrow();
     //errore anche se passo solo un parametro
-    experct(() => new User("Elisa")).toThrow(); //passo solo il nome
+    expect(() => new User("Elisa")).toThrow();
     //errore se passo solo il secondo parametro
-    expect(() => new User(null, 22)).toThrow(); //passo solo l'età
+    expect(() => new User(undefined, 23)).toThrow();
 });
 //controlla le proprietà name ed age siano della giusta tipologia
 //controlla le proprietà private
